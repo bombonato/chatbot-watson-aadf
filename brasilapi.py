88d8c1a409810e8a2263f15d1f919d99b28c6853
Fabio Mac <fabio.bombonato@gmail.com>
2022-04-09 18:19:23 -0300

    Telegram - correção url do serviço

--- brasilapi.py
+++ brasilapi.py
@@ -4,7 +4,7 @@ import os
 base_url = 'https://brasilapi.com.br/api'
 
 def get_cep(cep):
-    endpoint = '/cep/v2/' + cep
+    endpoint = base_url + '/cep/v2/' + cep
 
     response = requests.get(endpoint).json()
     if(response.status_code == requests.codes.ok):
